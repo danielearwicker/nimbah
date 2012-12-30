9f0315eef75007f2703febff7b014fdabd0b0c61
Daniel Earwicker <dan@earwicker.com>
2012-12-30 17:39:13 +0000

    Trying mongodb

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var app = express();
+var mongo = require('mongodb').MongoClient;
 
 app.use(express.static(__dirname));
 
+var connectionString = 'mongodb://nodejitsu:322270e3d4b21555bb6253d3233e5923@linus.mongohq.com:10069';
+
+
 app.get('/hellojoe', function(req, res){
-    var body = 'Hello Joe!';
+    /*var body = 'Hello Joe!';
     res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Content-Length', body.length);
-    res.end(body);
+    res.end(body);*/
+
+    mongo.connect(connectionString, function(err, db) {
+        if(!err) {
+            res.send("We are connected");
+        } else {
+            res.send(err.toString());
+        }
+     });
 });
 
 app.listen(process.env.PORT || 1337);
