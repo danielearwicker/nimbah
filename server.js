7afd065d201df02a1eac3e76af5416b70e273621
danielearwicker <dan@earwicker.com>
2012-12-30 21:35:36 +0000

    Trying janrain redirect

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var mongo = require('mongodb').MongoClient;
 
+var janrain = require('janrain-api'),
+    engageAPI = janrain('386e29baf51cc7eb88fe3da89ea2f514a4c9ac9a');
+
+var homepage = process.env.NIMBAH_HOMEPAGE || 'http://127.0.0.1';
+
 app.use(express.static(__dirname));
 
 var connectionString = 'mongodb://nodejitsu:322270e3d4b21555bb6253d3233e5923@linus.mongohq.com:10069/nodejitsudb8668093658';
@@ -14,6 +19,23 @@ app.get('/hellojoe', function(req, res){
             res.send('Got an error[2] - ' + err.toString());
         }
      });
+});
+
+app.get('/token/:token', function(req, res) {
+
+    if (req.params.token) {
+        res.send('No token');
+        return;
+    }
+
+    engageAPI.authInfo(req.params.token, true, function(err, data) {
+        if (err) {
+            console.log('ERROR: ' + err.message);
+            return;
+        }
+
+        req.redirect(homepage + '#login:' + data.profile.identifier);
+    });
 });
 
 app.listen(process.env.PORT || 1337);
