318d3e401ed0a01fc7846d7686a7355772510e15
danielearwicker <dan@earwicker.com>
2012-12-30 17:51:43 +0000

    fixed connection string

--- server.js
+++ server.js
@@ -4,15 +4,9 @@ var mongo = require('mongodb').MongoClient;
 
 app.use(express.static(__dirname));
 
-var connectionString = 'mongodb://nodejitsu:322270e3d4b21555bb6253d3233e5923@linus.mongohq.com:10069';
-
+var connectionString = 'mongodb://nodejitsu:322270e3d4b21555bb6253d3233e5923@linus.mongohq.com:10069/nodejitsudb8668093658';
 
 app.get('/hellojoe', function(req, res){
-    /*var body = 'Hello Joe!';
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Length', body.length);
-    res.end(body);*/
-
     mongo.connect(connectionString, function(err, db) {
         if(!err) {
             res.send("We are connected");
